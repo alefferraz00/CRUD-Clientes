43a658d54a434e112cf51b2b938ccd02490a3de7
Alef Ferraz <alef.ferraz00@gmail.com>
2022-03-08 18:32:24 -0300

    Incluidos a funções Update e Delete

--- src/main/java/com/crud/clientes/controller/ClienteController.java
+++ src/main/java/com/crud/clientes/controller/ClienteController.java
@@ -2,8 +2,12 @@ package com.crud.clientes.controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +24,31 @@ public class ClienteController {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping
+	@GetMapping("/clientes/lista")
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
 	
-	@PostMapping
+	@PostMapping("/cliente/add")
 	@ResponseStatus
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
+	
+	@PostMapping("/cliente/atualizar")
+	public Cliente atualizar(@PathVariable Long id, Cliente cliente) {
+	  clienteRepository.getOne(id);
+	  return clienteRepository.save(cliente);
+		
+	}
+	
+	@DeleteMapping("/cliente/delete")
+	public void delete (@PathVariable Long id) {
+		clienteRepository.deleteById(id);
+	}
+	
+	
+	
 	
 	
 }
--- src/main/java/com/crud/clientes/model/Cliente.java
+++ src/main/java/com/crud/clientes/model/Cliente.java
@@ -15,13 +15,13 @@ public class Cliente {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private long id;
-	
+	@Column (nullable = false)
 	private String nome;
 	
 	@Column (nullable = false)
 	private String cpf;
-	
+	@Column (nullable =false)
 	private String telefone;
-	
+	@Column (nullable = false)
 	private String endereco;
 }
